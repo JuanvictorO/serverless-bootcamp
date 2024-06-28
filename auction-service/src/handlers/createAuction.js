bf1bbd42669692841ccc8016bec71847add00e99
JuanvictorO <juanvictoroliveira2009@gmail.com>
2024-06-28 19:04:08 -0300

    feat: added dynamodb table and create item

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,7 +1,29 @@
+import { randomUUID } from 'crypto'
+import AWS from 'aws-sdk'
 
-export const handler = async (event, context) => {
+const dynamodb = new AWS.DynamoDB.DocumentClient()
+
+async function createAuction (event, context) {
+  const { title } = JSON.parse(event.body)
+  const now = new Date()
+  const uuid = randomUUID()
+
+  const auction = {
+    id: uuid,
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString()
+  }
+
+  await dynamodb.put({
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise()
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context })
+    statusCode: 201,
+    body: JSON.stringify(auction)
   }
 }
+
+export const handler = createAuction
